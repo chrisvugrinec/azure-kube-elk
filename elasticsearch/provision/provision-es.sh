28317af3f129c39932a01f389965c87ab13434e7
chris vugrinec <chris@air-chris.home>
2017-02-20 12:45:14 +0100

    changed to 2.4.0

--- elasticsearch/provision/provision-es.sh
+++ elasticsearch/provision/provision-es.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install -y java-1.8.0-openjdk.x86_64
-#wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.3.noarch.rpm
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.2.1.rpm
-rpm -ivh elasticsearch-5.2.1.rpm
+#wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.2.1.rpm
+wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.4.0/elasticsearch-2.4.0.rpm
+rpm -ivh elasticsearch-2.4.0.rpm
 systemctl enable elasticsearch.service
