5993abab0715f312a234d4cdd3d42e3f43521252
chris vugrinec <chris@air-chris.home>
2017-02-20 12:18:04 +0100

    upgraded elastic

--- elasticsearch/provision/provision-es.sh
+++ elasticsearch/provision/provision-es.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum install -y java-1.8.0-openjdk.x86_64
-wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.3.noarch.rpm
-rpm -ivh elasticsearch-1.7.3.noarch.rpm
+#wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.3.noarch.rpm
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.2.1.rpm
+rpm -ivh elasticsearch-5.2.1.rpm
 systemctl enable elasticsearch.service
